f828880735bb1cb95f159242b8507f71c4d86716
Le Quoc <lethanhquoc.net@gmail.com>
2024-04-08 15:55:46 +0700

    Event

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -8,12 +8,21 @@ class Mycomponent extends React.Component {
       age: 18,
       address: 'TG'
    };
+   handleClick(event) {
+      console.log('>> Click me');
+      console.log('My name is', this.state.name);
+   }
+   handleOnMouseEnter(event) {
+      console.log(event.pageX);
+   }
    render() {
       return (
          <div>
             <h1>My first Component</h1>
             <p>
                My name is {this.state.name} and I am form {this.state.address}
+               <button onClick={this.handleClick}>Click me</button>
+               <button onMouseOver={this.handleOnMouseEnter}>Hover me</button>
             </p>
          </div>
       );
